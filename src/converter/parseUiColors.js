5bb4b532b900dab45b2875518a8582aa83b80b82
Nick Eoneof <eoneof@gmail.com>
2022-06-09 17:36:56 +0300

    feat: parse ui colors

---
+++ src/converter/parseUiColors.js
@@ -0,0 +1,54 @@
+﻿const { readFileSync } = require('fs');
+
+const uiColors = JSON.parse(
+  readFileSync('./src/ase2json/ui-colors.json', 'utf-8'),
+);
+
+const tokens = {};
+
+/**
+ * Here we iterate through the array of objects
+ * and invert each object so that it's `elements` items
+ * becomes keys with value of the `colors` key of the current object o_O
+ */
+// uiColors.forEach((color) => {
+//   const colorName = Object.keys(color).toString();
+//   color[`${colorName}`].uiElements.forEach((element) => {
+//     // assign a color to an element
+//     tokens[element] = color[`${colorName}`].color;
+//   });
+// });
+
+// same as above but set token instead of color
+uiColors.forEach((color) => {
+  const colorName = Object.keys(color).toString();
+  color[`${colorName}`].uiElements.forEach((element) => {
+    // assign a color to an element
+    tokens[element] = colorName; //.color;
+  });
+});
+
+const colors = {
+  white_00: '#fff',
+  black_00: '#bcd',
+  black_01: '#dbc',
+};
+
+const paired = {};
+
+function matchValues(source, dest) {
+  return Object.entries(source).find(([key, val]) => {
+    if (key === undefined) {
+      return;
+    }
+    return dest === key; // ?
+  })[0];
+}
+
+for (let item in tokens) {
+  let result = matchValues(colors, tokens[item]);
+  paired[item] = colors[result]; // ?
+}
+
+// console.log(tokens);
+console.log(paired);
