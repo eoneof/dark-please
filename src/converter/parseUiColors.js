2e3d2a4c87af1f0d3243216da28c0e97cd331df7
Nick Eoneof <eoneof@gmail.com>
2022-06-10 17:40:32 +0300

    comment

--- src/converter/parseUiColors.js
+++ src/converter/parseUiColors.js
@@ -13,48 +13,49 @@ const tokens = {};
  * and invert each object so that it's `elements` items
  * becomes keys with value of the `colors` key of the current object o_O
  */
-// uiColors.forEach((color) => {
-//   const colorName = Object.keys(color).toString();
-//   color[`${colorName}`].uiElements.forEach((element) => {
-//     // assign a color to an element
-//     tokens[element] = color[`${colorName}`].color;
-//   });
-// });
-
-// same as above but set token instead of color
 uiColors.forEach((color) => {
   const colorName = Object.keys(color).toString();
   color[`${colorName}`].uiElements.forEach((element) => {
     // assign a color to an element
-    tokens[element] = colorName;
+    tokens[element] = color[`${colorName}`].color;
   });
 });
+
+// same as above but set token instead of color
+// uiColors.forEach((color) => {
+//   const colorName = Object.keys(color).toString();
+//   color[`${colorName}`].uiElements.forEach((element) => {
+//     // assign a color to an element
+//     tokens[element] = colorName;
+//   });
+// });
 
 console.log(tokens);
 
 // SUBSTITIUTE COLOR TOKENS WITH COLOR VALUES FROM FILE
 
 // mock data (get it from file later on)
-const colors = {
-  white_00: '#fff',
-  black_00: '#bcd',
-  black_01: '#dbc',
-};
+// const colors = {
+//   bek: '#f00f',
+//   white_00: '#fff',
+//   black_00: '#bcd',
+//   black_01: '#dbc',
+// };
 
-const paired = {};
+// const paired = {};
 
-function matchValues(source, dest) {
-  return Object.entries(source).find(([key, val]) => {
-    if (key === undefined) {
-      return;
-    }
-    return dest === key; // ?
-  })[0];
-}
+// function matchValues(source, dest) {
+//   return Object.entries(source).find(([key, val]) => {
+//     if (key === undefined) {
+//       return;
+//     }
+//     return dest === key; // ?
+//   })[0];
+// }
 
-for (let item in tokens) {
-  let result = matchValues(colors, tokens[item]);
-  paired[item] = colors[result]; // ?
-}
+// for (let item in tokens) {
+//   let result = matchValues(colors, tokens[item]);
+//   paired[item] = colors[result]; // ?
+// }
 
-console.log(paired);
+// console.log(paired);
